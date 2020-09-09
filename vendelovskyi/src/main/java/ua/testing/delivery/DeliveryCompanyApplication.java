f2e154f979dcd61d71742cafa8e157b9aff35f93
ivan <ivan_v123@ukr.net>
2020-09-09 10:30:27 +0300

    Project Ceated

---
+++ vendelovskyi/src/main/java/ua/testing/delivery/DeliveryCompanyApplication.java
@@ -0,0 +1,16 @@
+package ua.testing.delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * @author Vendelovskyi Ivan
+ * @version 1.0
+ */
+@SpringBootApplication
+public class DeliveryCompanyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryCompanyApplication.class, args);
+    }
+
+}
